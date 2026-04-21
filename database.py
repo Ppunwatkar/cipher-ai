3d71cf907bcdacb3e2666815e9f5c44e2bd6eab0
Prasanna Punwatkar <143747840+Ppunwatkar@users.noreply.github.com>
2026-04-21 18:52:44 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,20 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./cipher.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
+
+# 🔥 FIX for Railway (VERY IMPORTANT)
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    pool_pre_ping=True
 )
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
