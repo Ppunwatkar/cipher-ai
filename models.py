8c971303ae0d43b5e0e74b2652cf3a842121047c
Prasanna Punwatkar <143747840+Ppunwatkar@users.noreply.github.com>
2026-04-21 19:28:37 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,33 +1,21 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, Text
 from database import Base
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True)
     password = Column(String)
-
-    chats = relationship("Chat", back_populates="user")
-
 
 class Chat(Base):
     __tablename__ = "chats"
-
     id = Column(String, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"))
-
-    user = relationship("User", back_populates="chats")
-    messages = relationship("Message", back_populates="chat")
-
+    title = Column(String)
 
 class Message(Base):
     __tablename__ = "messages"
-
     id = Column(Integer, primary_key=True, index=True)
     chat_id = Column(String, ForeignKey("chats.id"))
     role = Column(String)
-    content = Column(String)
-
-    chat = relationship("Chat", back_populates="messages")
+    content = Column(Text)
